580cbf1de32bbaab0624033bd0a7c29d552b209d
Juan <juanwangwu@gmail.com>
2021-08-22 23:36:11 +0200

    Side bar

--- web/index.php
+++ web/index.php
@@ -1,23 +1,58 @@
 <html>
 <head>
     <link rel="stylesheet" href="index.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
 
 <body>
     <div id="wrapper">
         <div id="side_menu">
-            <p>
-            </p>
+            <div class="logo">
+                <i class='bx bx-menu' id="btn"></i>
+            </div>
+            <ul class="nav_list">
+                <li>
+                    <a href="#"><i class='bx bxs-grid-alt' ></i>
+                    <span class="links_name">Dashboard</span>
+                    <!--<span class="tooltip">Dashboard</span> -->
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><i class='bx bx-user' ></i>
+                    <span class="links_name">Perfil</span>
+                    <!--<span class="tooltip">Dashboard</span> -->
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><i class='bx bx-bar-chart-alt-2' ></i>
+                    <span class="links_name">Clasificación</span>
+                    <!--<span class="tooltip">Dashboard</span> -->
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><i class='bx bx-add-to-queue' ></i>
+                    <span class="links_name">Añadir</span>
+                    <!--<span class="tooltip">Dashboard</span> -->
+                    </a>
+                </li>
+                <li>
+                    <a href="#"><i class='bx bx-cog' ></i>
+                    <span class="links_name">Configuración</span>
+                    <!--<span class="tooltip">Dashboard</span> -->
+                    </a>
+                </li>
+            </ul>
         </div>
         <div id="container">
             <div id="search_bar">
-                <input type="text">
+                <input type="text" placeholder="Buscar">
                 <input type="submit">
             </div>
             <div id="content">
                 <div class="image">
                     <div class="image_menu">
+                        
                     </div>
                 </div>
                 <div class="image">
---
+++ web/menu.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Repositorio </title>
+        <link rel="stylesheet" href="style.css">
+        <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    </head>
+    <body>
+        <div class="sidebar">
+            <div class="logo_content">
+                <div class="logo">
+                    <i class="bx bx1-c-plus-plus"></i>
+                    <div class="logo_name">Repositorio</div>
+                </div>
+                <i class="bx bx-menu" id="btn"></i>
+            </div>
+            <ul class="nav_list">
+                <li>
+                    <a href="#">
+                        <i class="bx bx-grid-alt"></i>
+                        <span class="links_name">Dashboard</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-user' ></i>
+                        <span class="links_name">Perfil</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-bar-chart-alt-2' ></i>
+                        <span class="links_name">Clasificación</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-add-to-queue' ></i>
+                        <span class="links_name">Añadir</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                        <i class='bx bx-cog' ></i>
+                        <span class="links_name">Configuración</span>
+                    </a>
+                    <!--<span class="tooltip">Dashboard</span> -->
+                </li>
+            </ul>
+            <div class="profile_content">
+                <div class="profile">
+                    <div class="profile_details">
+                        <img src="#" alt="Logo">
+                        <div class="name_job">
+                            <div class="name">Rikuki</div>
+                            <div class="job">Ciber</div>
+                        </div>
+                    </div>
+                    <i class="bx bx-log-out" id="log_out"></id>
+                </div>
+            </div>
+
+    </body>
+</html>
